cd60c97eed9fe009546e35113761d8bd12885819
Aidin <aidin.forooghi@gmail.com>
2023-04-09 00:06:57 -0400

    Passed 4 Tests

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here!
+const mainNode = document.querySelector('main#main');
+if (mainNode) {
+    mainNode.remove();
+}
+
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.innerHTML = 'Aidin is the champion';
+
+document.body.appendChild(newHeader);
